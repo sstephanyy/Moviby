e1b7d1220c29c9464c7503dc279b1acf4500d14f
Stephanyy <santosstephany400@gmail.com>
2024-05-27 05:06:11 -0300

    Install tailwind

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,12 +3,10 @@ import { useState } from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <h1>Olá mundo</h1>
-    </>
+    <h1 className="text-3xl font-bold underline">
+      Hello world!
+    </h1>
   )
 }
 
